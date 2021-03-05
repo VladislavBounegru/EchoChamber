41c28f27aa1cc9562aa3f9f5af044e90858b60e8
VladislavBounegru <36441693+VladislavBounegru@users.noreply.github.com>
2021-03-05 13:12:25 +0300

    Add files via upload
    add files

---
+++ src/main/java/ru/bounegru/app/EchoChamber.java
@@ -0,0 +1,25 @@
+package ru.bounegru.app;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EchoChamber {
+    public static void main(String[] args) {
+        resolve();
+    }
+
+    private static void resolve() {
+        String line;
+        Scanner scanner = new Scanner(System.in);
+        List<String> lines = new ArrayList<>();
+
+        while (!(line = scanner.nextLine()).equals("")) {
+            lines.add(line);
+        }
+
+        for (String str: lines){
+            System.out.println(str);
+        }
+    }
+}
